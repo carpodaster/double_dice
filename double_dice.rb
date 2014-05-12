a1864df9ccce958a9dcd008ef47f081632b994eb
Carsten Zimmermann <cz@aegisnet.de>
2014-05-13 01:29:34 +0200

    First commit
    WIP: last line in #matrix is missing

---
+++ double_dice.rb
@@ -0,0 +1,47 @@
+class DoubleDice
+
+  attr_reader :cleartext, :password
+
+  def initialize(cleartext, password)
+    @cleartext = cleartext.upcase.strip.split(//).select{|c| /[A-Z]/ =~ c}
+    @password  = password.upcase.strip.split(//)
+  end
+
+  def matrix
+    matrix = [password]
+
+    line = []
+    cleartext.each do |char|
+      if line.size < password.size
+        line << char
+      else
+        matrix << line
+        line = [char]
+      end
+    end
+    matrix
+  end
+
+  def index_of(char)
+    ('A'..'Z').to_a.index(char)
+  end
+
+end
+
+
+slipsum = <<EOSLIPSUM
+The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
+EOSLIPSUM
+
+# print 'Please enter the cleartext: '
+# text = gets
+#
+# print 'Please enter a password: '
+# pass = gets
+
+# ddice = DoubleDice.new text, pass
+ddice = DoubleDice.new slipsum, 'foobar'
+
+puts ddice.cleartext.inspect
+puts
+puts ddice.matrix.map(&:inspect)
