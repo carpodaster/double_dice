c204307dfc85205ae2ae2c0d1e668ab25b4f3cfb
Carsten Zimmermann <carp@hacksocke.de>
2014-05-18 23:56:02 +0200

    WIP: Working on column shifter

--- double_dice.rb
+++ double_dice.rb
@@ -1,5 +1,25 @@
 class DoubleDice
 
+  class PasswordVector < Struct.new(:password)
+    def to_a
+      password.map{|char| ('A'..'Z').to_a.index(char) }.map(&:to_s)
+    end
+  end
+
+  class Shifter < Struct.new(:matrix)
+
+    def to_a
+      shifted_matrix = [matrix.password]
+    end
+
+    private
+
+    def vector
+      PasswordVector.new(matrix.password).to_a
+    end
+
+  end
+
   class Matrix < Struct.new(:cleartext, :password)
 
     def to_a
@@ -30,7 +50,7 @@ class DoubleDice
     end
 
     def index_line
-      password.map{|char| ('A'..'Z').to_a.index(char) }.map(&:to_s)
+      PasswordVector.new(password).to_a
     end
   end
 
@@ -42,7 +62,7 @@ class DoubleDice
   end
 
   def matrix
-    Matrix.new(cleartext, password).to_a
+    Matrix.new(cleartext, password)
   end
 
 
@@ -53,6 +73,13 @@ slipsum = <<EOSLIPSUM
 The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
 EOSLIPSUM
 
+one_ring = <<EOMIDDLEEARTH
+One Ring to rule them all,
+One Ring to find them,
+One ring to bring them all,
+And in the darkness bind them
+EOMIDDLEEARTH
+
 # print 'Please enter the cleartext: '
 # text = gets
 #
@@ -60,8 +87,13 @@ EOSLIPSUM
 # pass = gets
 
 # ddice = DoubleDice.new text, pass
-ddice = DoubleDice.new slipsum, 'foobar'
+# ddice = DoubleDice.new slipsum, 'foobar'
+ddice = DoubleDice.new one_ring, 'foobar'
 
 puts ddice.cleartext.inspect
 puts
-puts ddice.matrix.map(&:inspect)
+puts ddice.matrix.to_a.map(&:inspect)
+
+shifter = DoubleDice::Shifter.new(ddice.matrix)
+puts
+puts shifter.to_a.map(&:inspect)
