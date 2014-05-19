8506a93535c3d9f84242c2f5d7a88411edafc9f5
Carsten Zimmermann <cz@aegisnet.de>
2014-05-19 11:43:17 +0200

    Add specs for input transformation

--- double_dice.rb
+++ double_dice.rb
@@ -58,7 +58,7 @@ class DoubleDice
 
   def initialize(cleartext, password)
     @cleartext = cleartext.upcase.strip.split(//).select{|c| /[A-Z]/ =~ c}
-    @password  = password.upcase.strip.split(//)
+    @password  = password.upcase.strip.gsub(/\s/, '').split(//)
   end
 
   def matrix
--- spec/double_dice_spec.rb
+++ spec/double_dice_spec.rb
@@ -1,4 +1,51 @@
 require 'spec_helper'
 
 describe DoubleDice do
+  describe 'its constructor' do
+    context 'setting the cleartext' do
+      it 'sets the cleartext as character array' do
+        cleartext = 'FOOBAR'
+        subject = DoubleDice.new cleartext, ''
+
+        subject.cleartext.must_equal %w(F O O B A R)
+      end
+
+      it 'upcases the characters' do
+        cleartext = 'foobar'
+        subject = DoubleDice.new cleartext, ''
+
+        subject.cleartext.must_equal %w(F O O B A R)
+      end
+
+      it 'removes non-word characters' do
+        cleartext = "f00b_ r \n "
+        subject = DoubleDice.new cleartext, ''
+
+        subject.cleartext.must_equal %w(F B R)
+      end
+    end
+
+    context 'setting the password' do
+      it 'sets the password as character array' do
+        password = 'FOOBAZ'
+        subject = DoubleDice.new '', password
+
+        subject.password.must_equal %w(F O O B A Z)
+      end
+
+      it 'upcases the characters' do
+        password = 'foobar'
+        subject = DoubleDice.new '', password
+
+        subject.password.must_equal %w(F O O B A R)
+      end
+
+      it 'removes whitespaces' do
+        password = "f00b_ r \n "
+        subject = DoubleDice.new '', password
+
+        subject.password.must_equal %w(F 0 0 B _ R)
+      end
+    end
+  end
 end
