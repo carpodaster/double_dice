66a3f9396936114c2e4c745bbd1283622fb94265
Carsten Zimmermann <cz@aegisnet.de>
2014-09-05 19:52:05 +0200

    Finalize lint test

--- double_dice.rb
+++ double_dice.rb
@@ -4,6 +4,15 @@ require 'password_vector'
 
 module DoubleDice
 
+  def encrypt(message: message, password1: password1, password2: password2)
+    matrix1 = DoubleDice::Input.new(message, password1).matrix.sort
+    shifter = Shifter.new matrix1
+
+    matrix2 = DoubleDice::Input.new(shifter.to_s, password2).matrix.sort
+    Shifter.new(matrix2).to_s
+  end
+  module_function :encrypt
+
   class Shifter < Struct.new(:matrix)
 
     def to_s
--- spec/double_dice_spec.rb
+++ spec/double_dice_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
+describe DoubleDice do
+  it 'encrypts the lint text' do
+    message = 'HALLODASHIERISTEINLANGERBEISPIELTEXTUMDASVERFAHRENZUZEIGEN'
+    expected = [
+      %w(N O I U P F),
+      %w(R Z L S A T),
+      %w(G A R H H I),
+      %w(S B T E E A),
+      %w(E I N V E N),
+      %w(S N D I G M),
+      %w(A A E E T E),
+      %w(I D H E L L),
+      %w(E I X _ Z _)
+    ]
+
+    expected = 'NRSGS ESAIE OZRAB INADI ILURT NDEHX USRHE VIEEP AEHEE GTLZF TLIAN MEL'.tr(' ', '')
+
+    DoubleDice.encrypt(
+      message: message,
+      password1: 'NOTEBOOK',
+      password2: 'DECKEL'
+    ).must_equal expected
+  end
+end
+
 describe DoubleDice::Input do
   describe 'its constructor' do
     context 'setting the cleartext' do
